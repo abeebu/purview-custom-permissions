0f14338e21a4cba7f021b466f646e1483d83d43e
Abeeb Amoo <abeebamoo@microsoft.com>
2023-01-31 00:29:32 +0000

    checkpoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@
 set -e
 source ./functions-lib-to-set-purview-permissions.sh; 
 
+roles="${INPUT_ROLES}"
+echo "$INPUT_PURVIEW_NAME"
+echo $roles
+
 declare -A rolesMap
 rolesMap["data_reader"]='set-data-reader' 
 rolesMap["root_collection_admin"]='set-root-collection-admin' 
@@ -11,9 +15,9 @@ rolesMap["data_source_admin"]='set-data-source-admin'
 rolesMap["data_share_contributor"]='set-data-share-contributor' 
 rolesMap["workflow_admin"]='set-workflow-admin' 
 
-roles="${INPUT_ROLES}"
-
-echo $roles
+# roles="${INPUT_ROLES}"
+# echo "$INPUT_PURVIEW_NAME"
+# echo $roles
 
 #Assign each role passed in the argument
 for role in $roles
