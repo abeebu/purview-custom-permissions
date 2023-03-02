efc1708085f1c5d1eb000e3031b7e887029d116c
Abeeb Amoo <abeebamoo@microsoft.com>
2023-03-02 17:16:54 +0000

    Remove test workflow

--- functions-lib-to-set-purview-permissions.sh
+++ functions-lib-to-set-purview-permissions.sh
@@ -14,8 +14,6 @@ function set-permission() {
 
     purview_access_token=$(az account get-access-token --resource https://purview.azure.net/ --query accessToken --output tsv)
 
-    echo $purview_access_token
-
     body=$(curl -s -H "Authorization: Bearer $purview_access_token" "https://$1.purview.azure.com/policystore/collections/$1/metadataPolicy?api-version=2021-07-01")
     metadata_policy_id=$(echo "$body" | jq -r '.id')   
 
